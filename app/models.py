ac24c6cc31eb9d60fd49a38893398d832ca1490e
Dominic Hayden <dominichayden7@gmail.com>
2025-04-14 12:19:10 -0500

    Final

--- app/models.py
+++ app/models.py
@@ -1,1 +1,14 @@
 # Add any model classes for Flask-SQLAlchemy here
+
+from app import db
+from datetime import datetime, timezone
+
+class Movie(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    poster = db.Column(db.String(100), nullable=False)
+    created_at = db.Column(db.DateTime(timezone=True), nullable=False, default=lambda: datetime.now(timezone.utc))
+
+    def __repr__(self):
+        return f"Movie('{self.title}', '{self.poster}')"
